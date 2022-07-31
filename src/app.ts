a737569c5dffd75e3f62e7704be338c3fa1ff574
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-07-31 21:39:41 +0300

    add npm scripts and nodemon

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`swagger coming soon..`);
+});
+
+app.listen(port, () => {
+  console.log(`Express is listening at ${port}`);
+});
+
+export default app;
