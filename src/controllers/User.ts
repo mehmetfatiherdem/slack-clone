046b81a228d60b4b0c8ca964c7a7c7c38a373286
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-08-05 16:11:29 +0300

    change export style

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -1,23 +1,21 @@
 import { Request, Response } from 'express';
 
-const signInApp = (req: Request, res: Response) => {
+export const signInApp = (req: Request, res: Response) => {
   // sign in by entering your mail address and get a 6-digit code
   res.json({ message: 'sign in here' });
 };
 
-const signInWorkspace = (req: Request, res: Response) => {
+export const signInWorkspace = (req: Request, res: Response) => {
   // sign in to a workspace
   res.json({ message: 'sign in here' });
 };
 
-const signOutApp = (req: Request, res: Response) => {
+export const signOutApp = (req: Request, res: Response) => {
   res.clearCookie('app_token');
   res.json({ message: 'Signed out from the app' });
 };
 
-const signOutWorkspace = (req: Request, res: Response) => {
+export const signOutWorkspace = (req: Request, res: Response) => {
   // sign out from a workspace
   res.json({ message: 'Signed out from the workspace' });
 };
-
-export { signInApp, signOutApp, signOutWorkspace, signInWorkspace };
