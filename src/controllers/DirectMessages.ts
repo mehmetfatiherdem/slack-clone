df2b771dad1b61d7165026db983c366649b2cdda
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-08-21 16:52:24 +0300

    fix transaction in private message

--- src/controllers/DirectMessages.ts
+++ src/controllers/DirectMessages.ts
@@ -41,8 +41,6 @@ export const sendPrivateMessage = async (
     res.status(422).json({ message: 'Cannot send empty message' });
 
   const userRepo = AppDataSource.getRepository(User);
-  const directMessageRepo = AppDataSource.getRepository(DirectMessage);
-  const privateMessageRepo = AppDataSource.getRepository(PrivateMessage);
 
   const sender = await userRepo.findOne({
     where: { id: req.user.id },
@@ -62,53 +60,55 @@ export const sendPrivateMessage = async (
     ],
   });
 
-  if (!sender.directMessage) {
-    const directMessage = new DirectMessage();
-    directMessage.owner = sender;
-    directMessage.users = [];
-    directMessage.users.push(receiver);
-    sender.directMessage = directMessage;
-    await directMessageRepo.save(directMessage);
-  } else {
-    sender.directMessage.users.push(receiver);
-    await directMessageRepo.save(sender.directMessage);
-  }
+  await AppDataSource.transaction(async (transactionalEntityManager) => {
+    if (!sender.directMessage) {
+      const directMessage = new DirectMessage();
+      directMessage.owner = sender;
+      directMessage.users = [];
+      directMessage.users.push(receiver);
+      sender.directMessage = directMessage;
+      await transactionalEntityManager.save(directMessage);
+    } else {
+      sender.directMessage.users.push(receiver);
+      await transactionalEntityManager.save(sender.directMessage);
+    }
 
-  if (!receiver.directMessage) {
-    const directMessage = new DirectMessage();
-    directMessage.owner = receiver;
-    directMessage.users = [];
-    directMessage.users.push(sender);
-    receiver.directMessage = directMessage;
-    await directMessageRepo.save(directMessage);
-  } else {
-    receiver.directMessage.users.push(sender);
-    await directMessageRepo.save(receiver.directMessage);
-  }
+    if (!receiver.directMessage) {
+      const directMessage = new DirectMessage();
+      directMessage.owner = receiver;
+      directMessage.users = [];
+      directMessage.users.push(sender);
+      receiver.directMessage = directMessage;
+      await transactionalEntityManager.save(directMessage);
+    } else {
+      receiver.directMessage.users.push(sender);
+      await transactionalEntityManager.save(receiver.directMessage);
+    }
 
-  const privateMessage = new PrivateMessage();
-  privateMessage.text = text;
-  privateMessage.receiver = receiver;
-  privateMessage.sender = sender;
-  privateMessage.directMessages = [];
-  privateMessage.directMessages.push(sender.directMessage);
-  privateMessage.directMessages.push(receiver.directMessage);
+    const privateMessage = new PrivateMessage();
+    privateMessage.text = text;
+    privateMessage.receiver = receiver;
+    privateMessage.sender = sender;
+    privateMessage.directMessages = [];
+    privateMessage.directMessages.push(sender.directMessage);
+    privateMessage.directMessages.push(receiver.directMessage);
 
-  if (sender.privateMessagesSent?.length > 0) {
-    sender.privateMessagesSent.push(privateMessage);
-  } else {
-    sender.privateMessagesSent = [];
-    sender.privateMessagesSent.push(privateMessage);
-  }
+    if (sender.privateMessagesSent?.length > 0) {
+      sender.privateMessagesSent.push(privateMessage);
+    } else {
+      sender.privateMessagesSent = [];
+      sender.privateMessagesSent.push(privateMessage);
+    }
 
-  if (receiver.privateMessagesReceived?.length > 0) {
-    receiver.privateMessagesReceived.push(privateMessage);
-  } else {
-    receiver.privateMessagesReceived = [];
-    receiver.privateMessagesReceived.push(privateMessage);
-  }
+    if (receiver.privateMessagesReceived?.length > 0) {
+      receiver.privateMessagesReceived.push(privateMessage);
+    } else {
+      receiver.privateMessagesReceived = [];
+      receiver.privateMessagesReceived.push(privateMessage);
+    }
 
-  await privateMessageRepo.save(privateMessage);
+    await transactionalEntityManager.save(privateMessage);
+  });
 
   res.json({ message: 'private message sent' });
 };
