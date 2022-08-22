6cbc119e49ffd8a55a09019714dd5e6d81a200e6
Mehmet Fatih Erdem <mehmeterdem1024@gmail.com>
2022-08-22 17:16:49 +0300

    connect with db url

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,27 +9,7 @@ import { WorkSpace } from './entity/WorkSpace';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  url: process.env.NODE_ENV == 'production' ? process.env.DATABASE_URL : '',
-  host:
-    process.env.NODE_ENV == 'production'
-      ? process.env.CLOUD_DB_HOST
-      : 'localhost',
-  port:
-    process.env.NODE_ENV == 'production'
-      ? parseInt(process.env.CLOUD_DB_PORT)
-      : parseInt(process.env.DB_PORT),
-  username:
-    process.env.NODE_ENV == 'production'
-      ? process.env.CLOUD_DB_USERNAME
-      : process.env.DB_USERNAME,
-  password:
-    process.env.NODE_ENV == 'production'
-      ? process.env.CLOUD_DB_PASSWORD
-      : process.env.DB_PASSWORD,
-  database:
-    process.env.NODE_ENV == 'production'
-      ? process.env.CLOUD_DB_NAME
-      : process.env.DB_NAME,
+  url: process.env.NODE_ENV == 'production' ? process.env.DATABASE_URL : process.env.DB_URL,
   synchronize: true,
   logging: false,
   entities: [User, Channel, DirectMessage, Message, WorkSpace, PrivateMessage],
